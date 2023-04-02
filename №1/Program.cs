87ad925769b455dde64d8912258de6bd17092661
Vasil <vasilgm@yandex.ru>
2023-04-02 21:41:25 +0300

    two №2

--- №1/Program.cs
+++ №1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = number();
+if (N >=10000 && N<=99999)
+{
+    if(polindrom(N)) System.Console.WriteLine($"Число {N} является полиндромом");
+    else System.Console.WriteLine($"Число {N} не является полиндромом");
+}
+else System.Console.WriteLine($"Число {N} не пятизначное");
+
+int number ()
+{
+    System.Console.WriteLine("Введите число N:");
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+
+bool polindrom (int N)
+{
+    int N1 = N / 10000;
+    int N2 = N % 10;
+    int N3 = N / 1000 % 10;
+    int N4 = N / 10 % 10;
+    return N1 == N2 || N3 == N4;
+}
