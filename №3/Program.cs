1a0e126320f0c26df8150f5e34b5fc74a385f45f
Vasil <vasilgm@yandex.ru>
2023-04-02 22:35:14 +0300

    two №3

--- №3/Program.cs
+++ №3/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number()
+{
+    Console.Write($"Введите число N: ");
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+
+int N = number();
+cubs(N);
+
+
+void cubs (int N)
+{
+    for (int i=1; i<=N; i++)
+    {
+     System.Console.WriteLine($"Куб числа {i} равен {Math.Pow(i, 3)}");
+    }
+}
